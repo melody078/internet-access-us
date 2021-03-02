5222209c66bf020857f4be6b5905b295f543dd4e
Melody Zhu <melodyyzhu@gmail.com>
2021-03-01 23:30:48 -0800

    add datasets and question files

---
+++ main.py
@@ -0,0 +1,9 @@
+# import research question python files
+
+def main():
+  # load in data from csv files
+  # pass data to corresponding research question method
+  print('test')
+
+if __name__ == '__main__':
+  main()
