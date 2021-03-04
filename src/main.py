cf4087ab11dd41c5c2b75780827a93b64a90191d
Melody Zhu <melodyyzhu@gmail.com>
2021-03-03 23:11:55 -0800

    testing dataset

--- src/main.py
+++ src/main.py
@@ -4,21 +4,21 @@ import q1
 
 def main():
   # load in data from csv files
-  data_1_2015 = pd.read_csv('../data/dataset-1-2015.csv')
-  data_1_2016 = pd.read_csv('../data/dataset-1-2016.csv')
-  data_1_2017 = pd.read_csv('../data/dataset-1-2017.csv')
-  data_1_2018 = pd.read_csv('../data/dataset-1-2018.csv')
-  data_1_2019 = pd.read_csv('../data/dataset-1-2019.csv')
+  data_1_2015 = pd.read_csv('data/dataset-1-2015.csv')
+  data_1_2016 = pd.read_csv('data/dataset-1-2016.csv')
+  data_1_2017 = pd.read_csv('data/dataset-1-2017.csv')
+  data_1_2018 = pd.read_csv('data/dataset-1-2018.csv')
+  data_1_2019 = pd.read_csv('data/dataset-1-2019.csv')
 
-  data_2_2015 = pd.read_csv('../data/dataset-2-2015.csv')
-  data_2_2016 = pd.read_csv('../data/dataset-2-2016.csv')
-  data_2_2017 = pd.read_csv('../data/dataset-2-2017.csv')
-  data_2_2018 = pd.read_csv('../data/dataset-2-2018.csv')
-  data_2_2019 = pd.read_csv('../data/dataset-2-2019.csv')
+  data_2_2015 = pd.read_csv('data/dataset-2-2015.csv')
+  data_2_2016 = pd.read_csv('data/dataset-2-2016.csv')
+  data_2_2017 = pd.read_csv('data/dataset-2-2017.csv')
+  data_2_2018 = pd.read_csv('data/dataset-2-2018.csv')
+  data_2_2019 = pd.read_csv('data/dataset-2-2019.csv')
 
-  data_3 = pd.read_csv('../data/dataset-3.csv')
-  data_4 = pd.read_csv('../data/dataset-4.csv')
-  data_5 = pd.read_csv('../data/dataset-5.csv')
+  data_3 = pd.read_csv('data/dataset-3.csv')
+  data_4 = pd.read_csv('data/dataset-4.csv')
+  data_5 = pd.read_csv('data/dataset-5.csv')
 
   # clean up datasets
 
